db2ff43d2193eef71dd5a79fa21f9a387e04ef91
petrlev1 <levendeevp@gmail.com>
2023-09-20 19:38:42 +0300

    Edit4

--- src/app/tab2/dostavka-preparatovi/dostavka-preparatovi.page.ts
+++ src/app/tab2/dostavka-preparatovi/dostavka-preparatovi.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DostavkaPreparatoviPage implements OnInit {
 
+  public sentButtons = ['Хорошо'];
+
   constructor() { }
 
   ngOnInit() {
