55d8b0d2b83de11dab991011d2455d6c5fbdc1bf
Тимофей <timalev@mail.ru>
2023-10-26 14:42:14 +0300

    auth

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { getAuth,createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+
+import { getDatabase, ref, set } from "firebase/database";
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +11,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+
+loginForm: any = {
+  login: '',
+  password: '',
+}
+
+  constructor() {
+
+  // tabs/tab5/zakazat-test
+
+
+   const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+
+    console.log('Reged!!');
+    //this.html_form = 0;
+    //this.ProfName = "Privet " + uid + " !";
+  //  this.ProfName = "Добро пожаловать!";
+
+
+
+  } else {
+    // User is signed out
+
+   console.log('not reged');
+//   this.html_form = 1;
+
+  }
+});
+
+
+  }
 
   ngOnInit() {
   }
+
+  SignIn(){
+
+
+   const auth = getAuth();
+signInWithEmailAndPassword(auth, this.loginForm.login, this.loginForm.password)
+  .then((userCredential) => {
+
+ // this.isValid = false;
+    // Signed in
+    const user = userCredential.user;
+    // ...
+
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  //  this.isValid = true;
+
+    //alert(errorMessage);
+    console.log(errorMessage);
+  });
+
+}
+
+
+
+
+
 
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,25 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3wqDxm-jJPnFM0n98ncETGXYZs-YWmK8",
+  authDomain: "medic-fc8ac.firebaseapp.com",
+  projectId: "medic-fc8ac",
+  storageBucket: "medic-fc8ac.appspot.com",
+  messagingSenderId: "152928306196",
+  appId: "1:152928306196:web:aebe15be6d5542ab251301"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 if (environment.production) {
   enableProdMode();
 }
