21d2eec6ca847cd5e8f0106ecd19067d7711408a
Тимофей <timalev@mail.ru>
2023-12-29 00:25:35 +0300

    otmena zapisi 2

--- src/app/tab2/zapisi-detail/zapisi-detail.page.ts
+++ src/app/tab2/zapisi-detail/zapisi-detail.page.ts
@@ -24,16 +24,18 @@ doctor: string;
 time: string;
 date: string;
 cancel: number;
-  
+loader: boolean;
+
   constructor(private http: HttpClient, private route: ActivatedRoute) {
-	  
+
 	    this.id = "";
 	  this.doctor = "";
 	  this.time = "";
 	  this.date = "";
 	  this.cancel = 0;
-  
-  
+	  this.loader = false;
+
+
   }
 
   ngOnInit() {
@@ -51,38 +53,44 @@ cancel: number;
 		if (params['cancel']==1)
 		{
 			//console.log("go api");
-			
+
 			onAuthStateChanged(auth, (user) => {
 				if (user) {
-					
+
 					const uid = user.uid;
 					const starCountRef = ref(getDatabase(), '/users/' + uid + "/auth");
-					
+
 					onValue(starCountRef, (snapshot) => {
-						
+
 						const clogin = snapshot.val().login;
 						const cpassw = snapshot.val().passw;
-						
+
 						//console.log(id + " / " + clogin + " / " +cpassw);
-						
+
 						const config = {
 							headers: {
 								//  'Content-Type':  'application/json',
 								//  'Authorization': 'Basic ' + btoa(unescape(encodeURIComponent('Администратор:By3dy9di')
 								}
 							};
-						
+
 						console.log(params['id'] + " / " + clogin + " / " + sha1(cpassw));
 
 						this.http.post('https://rieltorov.net/tmp/medicapi3zapisd.php', {id: params['id'], clogin: clogin, cpassw: sha1(cpassw)})
-							
+
 						.subscribe(
 							data => { // json data
 							//  this.zapismess = "<font color=green><i>Вы успешно записаны к доктору " + doctor + " на " + date + " в " + time + "</i></font>";
-							
+
 							   //console.log(Object.values(data)[1]);
-							   console.log(data);
-							
+							   console.log(Object.values(data)[1]);
+
+							   if (Object.values(data)[1]==true)
+							   {
+							        this.loader = true;
+							        window.location.href = '/tabs/tab2';
+							   }
+
 							   //this.titlesList = Object.values(data);
 							/*
 							   if (Object.values(data)[1]==true)
@@ -118,8 +126,8 @@ cancel: number;
   );
 
 }
-  
-  
-  
-  
+
+
+
+
  }
